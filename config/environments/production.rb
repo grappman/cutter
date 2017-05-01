8153882e590629db85f9543fa9c8ab813b60e29b
Sadyr Bagaadziev <grappman@yandex.ru>
2017-05-01 13:24:10 +0300

    Fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.routes.default_url_options[:host] = 'https://compress-url.herokuapp.com/'
+
 end
